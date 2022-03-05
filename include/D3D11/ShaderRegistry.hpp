a0517fc5bde31520b859a10e323890d18b72f90a
DarebacK <dareback1@gmail.com>
2022-03-05 13:10:40 +0100

    ShaderRegistry work in progress 5.

--- include/D3D11/ShaderRegistry.hpp
+++ include/D3D11/ShaderRegistry.hpp
@@ -1,9 +1,12 @@
 #pragma once
 
 #include "Core/Core.hpp"
+#include "Core/File.hpp"
 #include "D3D11/D3D11.hpp"
 
 #include <vector>
+
+#include <d3dcompiler.h>
 
 class TaskScheduler;
 
@@ -26,11 +29,6 @@ namespace D3D11
     ID3D11VertexShader* findVertexShader(LPCTSTR name) const;
     ID3D11PixelShader* findPixelShader(LPCTSTR name) const;
 
-  protected:
-
-    // TODO: replace with DEFINE_INPUT_ELEMENT_DESCRIPTIONS macro?
-    virtual void getInputElementDescriptions(const wchar_t* vertexShaderName, const D3D11_INPUT_ELEMENT_DESC** descriptions, int64* count) = 0;
-
   private:
     
     static constexpr int shaderCountMax = 50;
@@ -50,12 +48,50 @@ namespace D3D11
       void addName(LPCTSTR fileName);
     };
 
-    struct VertexShaders : public Shaders<ID3D11VertexShader>
+    struct VertexShaders
     {
-      CComPtr<ID3D11InputLayout> inputLayouts[shaderCountMax];
+      #define VERTEX_SHADER(name, ...) + 1
+      static constexpr int64 count = 0
+        #include VERTEX_SHADERS
+        ;
+      #undef VERTEX_SHADER
+
+      #define VERTEX_SHADER(name, ...) L ## #name ,
+      static constexpr const wchar_t* const names[] = {
+        #include VERTEX_SHADERS
+      };
+      #undef VERTEX_SHADER
+
+      #define VERTEX_SHADER(name, ...) nullptr,
+      CComPtr<ID3D11VertexShader> handles[count] = {
+        #include VERTEX_SHADERS
+      };
+      #undef VERTEX_SHADER
+
+      #define VERTEX_SHADER(name, ...) nullptr,
+      CComPtr<ID3D11InputLayout> inputLayouts[count] = {
+        #include VERTEX_SHADERS
+      };
+      #undef VERTEX_SHADER
+
+      #define VERTEX_SHADER(name, ...) static constexpr D3D11_INPUT_ELEMENT_DESC name ## ElementDescription[] = { __VA_ARGS__ };
+        #include VERTEX_SHADERS
+      #undef VERTEX_SHADER
+
+      struct InputElementEntry
+      {
+        const D3D11_INPUT_ELEMENT_DESC* const descriptions;
+        int64 count;
+      };
+      #define VERTEX_SHADER(name, ...) { name ## ElementDescription , arrayLength( name ## ElementDescription )},
+      static constexpr InputElementEntry inputElementEntries[count] = {
+        #include VERTEX_SHADERS
+      };
+      #undef VERTEX_SHADER
     };
 
     VertexShaders vertexShaders;
+
     Shaders<ID3D11PixelShader> pixelShaders;
 
     void reset();
@@ -71,4 +107,375 @@ namespace D3D11
     void createPixelShader(LPCTSTR fileName, const void* shaderBytecode, uint64 shaderBytecodeSize);
 
   };
+
+  // IMPLEMENTATION DETAILS BELOW
+
+  constexpr UINT compileFlags = 0
+#ifdef DAR_DEBUG
+    | D3DCOMPILE_DEBUG
+    | D3DCOMPILE_SKIP_OPTIMIZATION
+#else
+    | D3DCOMPILE_OPTIMIZATION_LEVEL3
+#endif
+    | D3DCOMPILE_PACK_MATRIX_ROW_MAJOR
+    ;
+
+  void ShaderRegistry::reload(LPCTSTR shaderDirectoryPath)
+  {
+    reset();
+
+    TCHAR searchQuery[256];
+    _tcscpy_s(searchQuery, shaderDirectoryPath);
+
+    size_t searchQueryPathLength = _tcslen(shaderDirectoryPath);
+    searchQuery[searchQueryPathLength++] = L'\\';
+    searchQuery[searchQueryPathLength++] = L'*';
+    searchQuery[searchQueryPathLength++] = L'\0';
+
+    std::vector<byte> shaderBytecode;
+    load(searchQuery, shaderBytecode);
+  }
+
+  void ShaderRegistry::reset()
+  {
+    for (int64 i = 0; i < vertexShaders.count; i++)
+    {
+      vertexShaders.handles[i] = nullptr;
+      vertexShaders.inputLayouts[i] = nullptr;
+    }
+    pixelShaders.reset();
+  }
+
+  void createShaderFilePath(LPCTSTR shaderDirectoryPath, TCHAR* shaderFileName, TCHAR* destination)
+  {
+    int64 directoryPathLength = 0;
+    while (*(shaderDirectoryPath + directoryPathLength) != L'\0')
+    {
+      destination[directoryPathLength++] = *(shaderDirectoryPath + directoryPathLength);
+    }
+    _tcscpy_s(destination + directoryPathLength - 1, 256 - directoryPathLength + 1, shaderFileName); // -1/+1 because of cutting of the wildcard star.
+  }
+
+  template <typename ShadersType>
+  int64 findShader(LPCTSTR shaderFileName, ShadersType& shaders)
+  {
+    for (int64 shaderIndex = 0; shaderIndex < shaders.count; ++shaderIndex)
+    {
+      LPCTSTR shaderName = shaders.names[shaderIndex];
+      int64 shaderFileNameCharOffset = 0;
+
+      while (*shaderName != L'\0')
+      {
+        if (*shaderName != *(shaderFileName + shaderFileNameCharOffset++))
+        {
+          break;
+        }
+        shaderName++;
+      }
+
+      if (*shaderName == L'\0' && *(shaderFileName + shaderFileNameCharOffset) == L'.')
+      {
+        return shaderIndex;
+      }
+    }
+
+    return -1;
+  }
+
+#define isChar(x) (fileData.cFileName[charIndex] == x)
+#define isFileNameEnd() (fileData.cFileName[charIndex] == L'\0')
+#define logInvalidFile() logError("Invalid file in shader directory: %S", fileData.cFileName)
+
+  void ShaderRegistry::load(LPCTSTR shaderDirectoryPath, std::vector<byte>& shaderBytecode)
+  {
+    WIN32_FIND_DATA fileData;
+    void* searchHandle = FindFirstFile(shaderDirectoryPath, &fileData);
+    if (searchHandle == INVALID_HANDLE_VALUE)
+    {
+      logError("Failed to reload shaders. Couldn't find first file in %S.", shaderDirectoryPath);
+      return;
+    }
+
+    do
+    {
+      int32 charIndex = 0;
+
+      if (fileData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
+      {
+        if (!isFileNameEnd() && !isChar(L'.'))
+        {
+          TCHAR searchQuery[256];
+          wcscpy_s(searchQuery, shaderDirectoryPath);
+
+          searchQuery[_tcslen(searchQuery) - 1] = L'\0'; // Remove the star from wildcard
+
+          _tcscat_s(searchQuery, fileData.cFileName);
+
+          size_t searchQueryLength = _tcslen(searchQuery);
+          searchQuery[searchQueryLength++] = L'\\';
+          searchQuery[searchQueryLength++] = L'*';
+          searchQuery[searchQueryLength] = L'\0';
+
+          load(searchQuery, shaderBytecode);
+        }
+      }
+      else
+      {
+        const size_t fileNameLength = _tcsclen(fileData.cFileName);
+
+        while (true)
+        {
+          if (isChar(L'.'))
+          {
+            charIndex++;
+            if (isChar(L'v'))
+            {
+              charIndex++;
+              if (isChar(L's'))
+              {
+                charIndex++;
+                if (isChar(L'.'))
+                {
+                  charIndex++;
+                  const size_t remainingCharacterLength = fileNameLength - charIndex;
+                  if (remainingCharacterLength == 3)
+                  {
+                    if (fileData.cFileName[charIndex] == L'c' && fileData.cFileName[charIndex + 1] == L's' && fileData.cFileName[charIndex + 2] == L'o')
+                    {
+                      TCHAR filePath[256];
+                      createShaderFilePath(shaderDirectoryPath, fileData.cFileName, filePath);
+                      loadVertexShaderObjectFile(filePath, fileData.cFileName, shaderBytecode);
+                      break;
+                    }
+                    else
+                    {
+                      logInvalidFile();
+                      break;
+                    }
+                  }
+                  else if (remainingCharacterLength == 4)
+                  {
+                    if (fileData.cFileName[charIndex] == L'h' && fileData.cFileName[charIndex + 1] == L'l' && fileData.cFileName[charIndex + 2] == L's' && fileData.cFileName[charIndex + 3] == L'l')
+                    {
+                      TCHAR filePath[256];
+                      createShaderFilePath(shaderDirectoryPath, fileData.cFileName, filePath);
+                      loadVertexShaderSourceFile(filePath, fileData.cFileName);
+                    }
+                    else
+                    {
+                      logInvalidFile();
+                      break;
+                    }
+                  }
+                  else
+                  {
+                    logInvalidFile();
+                    break;
+                  }
+                }
+                else
+                {
+                  logInvalidFile();
+                  break;
+                }
+              }
+              else
+              {
+                logInvalidFile();
+                break;
+              }
+            }
+            else if (isChar(L'p'))
+            {
+              charIndex++;
+              if (isChar(L's'))
+              {
+                charIndex++;
+                if (isChar(L'.'))
+                {
+                  charIndex++;
+                  const size_t remainingCharacterLength = fileNameLength - charIndex;
+                  if (remainingCharacterLength == 3)
+                  {
+                    if (fileData.cFileName[charIndex] == L'c' && fileData.cFileName[charIndex + 1] == L's' && fileData.cFileName[charIndex + 2] == L'o')
+                    {
+                      TCHAR filePath[256];
+                      createShaderFilePath(shaderDirectoryPath, fileData.cFileName, filePath);
+                      loadPixelShaderObjectFile(filePath, fileData.cFileName, shaderBytecode);
+                      break;
+                    }
+                    else
+                    {
+                      logInvalidFile();
+                      break;
+                    }
+                  }
+                  else if (remainingCharacterLength == 4)
+                  {
+                    if (fileData.cFileName[charIndex] == L'h' && fileData.cFileName[charIndex + 1] == L'l' && fileData.cFileName[charIndex + 2] == L's' && fileData.cFileName[charIndex + 3] == L'l')
+                    {
+                      TCHAR filePath[256];
+                      createShaderFilePath(shaderDirectoryPath, fileData.cFileName, filePath);
+                      loadPixelShaderSourceFile(filePath, fileData.cFileName);
+                    }
+                    else
+                    {
+                      logInvalidFile();
+                      break;
+                    }
+                  }
+                }
+                else
+                {
+                  logInvalidFile();
+                  break;
+                }
+              }
+              else
+              {
+                logInvalidFile();
+                break;
+              }
+            }
+            else
+            {
+              logInvalidFile();
+              break;
+            }
+          }
+          else if (isFileNameEnd())
+          {
+            logInvalidFile();
+            break;
+          }
+          else
+          {
+            charIndex++;
+          }
+        }
+      }
+    } while (FindNextFile(searchHandle, &fileData));
+  }
+
+  void ShaderRegistry::loadVertexShaderSourceFile(LPCTSTR filePath, LPCTSTR fileName)
+  {
+    CComPtr<ID3DBlob> compiledCode;
+    if (FAILED(D3DCompileFromFile(filePath, nullptr, D3D_COMPILE_STANDARD_FILE_INCLUDE, "vertexShaderMain", "vs_5_0", compileFlags, 0, &compiledCode, nullptr)))
+    {
+      logError("Failed to compile vertex shader %S", filePath);
+      return;
+    }
+
+    createVertexShader(fileName, compiledCode->GetBufferPointer(), compiledCode->GetBufferSize());
+  }
+
+  void ShaderRegistry::loadVertexShaderObjectFile(LPCTSTR filePath, LPCTSTR fileName, std::vector<byte>& shaderBytecode)
+  {
+    if (!tryReadEntireFile(filePath, shaderBytecode))
+    {
+      logError("Failed to load vertex shader object file %S", filePath);
+      return;
+    }
+
+    createVertexShader(fileName, shaderBytecode.data(), shaderBytecode.size());
+  }
+
+  void ShaderRegistry::createVertexShader(LPCTSTR fileName, const void* shaderBytecode, uint64 shaderBytecodeSize)
+  {
+    int64 shaderIndex = findShader(fileName, vertexShaders);
+    if (shaderIndex < 0)
+    {
+      logError("There is no vertex shader %S", fileName);
+      return;
+    }
+
+    if (FAILED(device->CreateVertexShader(shaderBytecode, shaderBytecodeSize, nullptr, &vertexShaders.handles[shaderIndex])))
+    {
+      logError("Failed to create vertex shader %S", fileName);
+      return;
+    }
+
+    const D3D11_INPUT_ELEMENT_DESC* inputElementDescriptions = vertexShaders.inputElementEntries[shaderIndex].descriptions;
+    int64 inputElementDescriptionCount = vertexShaders.inputElementEntries[shaderIndex].count;
+
+    if (inputElementDescriptions == nullptr)
+    {
+      logError("Failed to get input element description for vertex shader %S", fileName);
+      return;
+    }
+
+    if (FAILED(device->CreateInputLayout(inputElementDescriptions, static_cast<UINT>(inputElementDescriptionCount), shaderBytecode, shaderBytecodeSize, &vertexShaders.inputLayouts[shaderIndex]))) {
+      logError("Failed to create input layout for vertex shader %S", fileName);
+      return;
+    }
+
+    logInfo("Successfully loaded vertex shader %S", fileName);
+  }
+
+  void ShaderRegistry::loadPixelShaderSourceFile(LPCTSTR filePath, LPCTSTR fileName)
+  {
+    CComPtr<ID3DBlob> compiledCode;
+    if (FAILED(D3DCompileFromFile(filePath, nullptr, D3D_COMPILE_STANDARD_FILE_INCLUDE, "pixelShaderMain", "ps_5_0", compileFlags, 0, &compiledCode, nullptr)))
+    {
+      logError("Failed to compile pixel shader %S", filePath);
+      return;
+    }
+
+    createPixelShader(fileName, compiledCode->GetBufferPointer(), compiledCode->GetBufferSize());
+  }
+
+  void ShaderRegistry::loadPixelShaderObjectFile(LPCTSTR filePath, LPCTSTR fileName, std::vector<byte>& shaderBytecode)
+  {
+    if (!tryReadEntireFile(filePath, shaderBytecode))
+    {
+      logError("Failed to load pixel shader object file %S", filePath);
+      return;
+    }
+
+    createPixelShader(fileName, shaderBytecode.data(), shaderBytecode.size());
+  }
+
+  void ShaderRegistry::createPixelShader(LPCTSTR fileName, const void* shaderBytecode, uint64 shaderBytecodeSize)
+  {
+    if (FAILED(device->CreatePixelShader(shaderBytecode, shaderBytecodeSize, nullptr, &pixelShaders.handles[pixelShaders.count])))
+    {
+      logError("Failed to create pixel shader %S", fileName);
+      return;
+    }
+
+    pixelShaders.addName(fileName);
+
+    pixelShaders.count++;
+
+    logInfo("Successfully loaded pixel shader %S", fileName);
+  }
+
+  template<typename ShaderType>
+  void ShaderRegistry::Shaders<ShaderType>::reset()
+  {
+    for (int64 i = 0; i < shaderCountMax; ++i)
+    {
+      handles[i] = nullptr;
+      names[i] = nullptr;
+      namesBufferEnd = 0;
+      count = 0;
+    }
+  }
+
+  template <typename ShaderType>
+  void ShaderRegistry::Shaders<ShaderType>::addName(LPCTSTR fileName)
+  {
+    int64 shaderNameLength = 0;
+    while (*fileName != L'\0' && *fileName != L'.') // Cut off name extensions.
+    {
+      namesBuffer[namesBufferEnd + shaderNameLength++] = *fileName;
+      fileName++;
+    }
+    namesBuffer[namesBufferEnd + shaderNameLength] = L'\0';
+    const wchar_t* shaderName = namesBuffer + namesBufferEnd;
+    names[count] = shaderName;
+    namesBufferEnd += shaderNameLength + 1;
+    assert(namesBufferEnd <= arrayLength(namesBuffer) + 1);
+  }
+
 }
