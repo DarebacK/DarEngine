8cd6273b4f710e4de9c292b60bf22f997f532547
DarebacK <dareback1@gmail.com>
2022-03-08 22:11:53 +0100

    Added simple static camera.

--- modules/D3D11/source/shaders/Terrain.vs.hlsl
+++ modules/D3D11/source/shaders/Terrain.vs.hlsl
@@ -5,12 +5,12 @@ cbuffer Transformation
 	float4x4 transform;
 };
 
-static float2 vertices[] =
+static float2 positions[] =
 {
-	float2(-1.f, -1.f),
-	float2(-1.f,  1.f),
+	float2( 0.f,  0.f),
+	float2( 0.f,  1.f),
 	float2( 1.f,  1.f),
-	float2( 1.f, -1.f),
+	float2( 1.f,  0.f),
 };
 
 struct Input
@@ -20,5 +20,6 @@ struct Input
 
 float4 vertexShaderMain(Input input) : SV_POSITION
 {
-	return mul(float4(vertices[input.vertexIndex], 0.f, 1.f), transform);
+	const float2 position = positions[input.vertexIndex];
+	return mul(float4(position.x, 0.f, position.y, 1.f), transform);
 }
