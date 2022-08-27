3810bd3204ec09045bb6b2ed4cb23dc5a676fe5c
DarebacK <dareback1@gmail.com>
2022-08-27 13:14:11 +0200

    Config parsing implementation. Not tested yet.

--- include/Core/Config.hpp
+++ include/Core/Config.hpp
@@ -1,2 +1,51 @@
 #pragma once
 
+#include <functional>
+
+#include "Core/Core.hpp"
+
+// Config files are essentialy .ini files, but they use .cfg extension to allow for potentional future format modifications. 
+
+struct ConfigKeyValueNode
+{
+  bool toBool() const;
+  int64 toInt() const;
+  float toFloat() const;
+  double toDouble() const;
+
+  char* key;
+  char* value;
+  const char* sectionPath;
+  int64 dataLength;
+  int64 valueLength;
+  int64 sectionPathLength;
+};
+using ConfigKeyValueNodeCallback = std::function<bool(const ConfigKeyValueNode&)>;
+
+struct ConfigSectionNode
+{
+  // TODO: Find out which fields are worth to keep. Are data and sectionPath same in this case?
+  char* data;
+  char* value;
+  const char* sectionPath;
+  int64 dataLength;
+  int64 valueLength;
+  int64 sectionPathLength;
+};
+using ConfigSectionNodeCallback = std::function<bool(const ConfigSectionNode&)>;
+
+
+/**
+ * Parses a config from a memory buffer. Calls callbacks for key/value pairs only.
+ * @param data buffer with the loaded config, WARNING: can be modified by the parser.
+ * @param nodeCallback called for each key/value pair node, return false to continue parsing, true to stop parsing.
+ */ 
+bool tryParseConfig(char* data, int64 dataLength, const ConfigKeyValueNodeCallback& nodeCallback);
+
+/**
+ * Parses a config from a memory buffer. Calls callbacks for both sections and key/value pairs.
+ * @param data buffer with the loaded config, WARNING: can be modified by the parser.
+ * @param sectionNodeCallback called for each section node, return false to continue parsing, true to stop parsing.
+ * @param keyValueNodeCallback called for each key/value pair node, return false to continue parsing, true to stop parsing.
+ */
+bool tryParseConfig(char* data, int64 dataLength, const ConfigSectionNodeCallback& sectionNodeCallback, const ConfigKeyValueNodeCallback& keyValueNodeCallback);
--- modules/Core/source/Config.cpp
+++ modules/Core/source/Config.cpp
@@ -1,5 +1,123 @@
 #define DAR_MODULE_NAME "Config"
 
-#include "Core/Core.hpp"
+#include "Core/Config.hpp"
+
 #include "external/libconfini/confini.h"
 
+bool ConfigKeyValueNode::toBool() const
+{
+  int retVal = ini_get_bool(value, -1);
+  if (retVal == -1)
+  {
+    logError("Failed to parse value %s with key %s in section %s as bool, returning false.", value, key, sectionPath);
+    return false;
+  }
+
+  return retVal;
+}
+
+int64 ConfigKeyValueNode::toInt() const
+{
+  return ini_get_llint(value);
+}
+
+float ConfigKeyValueNode::toFloat() const
+{
+  return ini_get_double(value);
+}
+
+double ConfigKeyValueNode::toDouble() const
+{
+  return ini_get_double(value);
+}
+
+static ConfigKeyValueNode toConfigKeyValueNode(IniDispatch* dispatch)
+{
+  ConfigKeyValueNode node;
+  node.key = dispatch->data;
+  node.value = dispatch->value;
+  node.sectionPath = dispatch->append_to;
+  node.dataLength = dispatch->d_len;
+  node.valueLength = dispatch->v_len;
+  node.sectionPathLength = dispatch->at_len;
+  return node;
+}
+
+static bool libconfiniParse(char* data, int64 dataLength, IniDispHandler callback, void* userData)
+{
+  switch (strip_ini_cache(data, size_t(dataLength), INI_DEFAULT_FORMAT, nullptr, callback, userData))
+  {
+    case CONFINI_SUCCESS:
+    case CONFINI_IINTR:
+    case CONFINI_FEINTR:
+      return true;
+
+    default:
+      return false;
+  }
+}
+
+static int parseKeyValueCallback(IniDispatch* dispatch, void* userData)
+{
+  if (dispatch->type == INI_KEY)
+  {
+    const ConfigKeyValueNodeCallback& nodeCallback = *(ConfigKeyValueNodeCallback*)userData;
+    return nodeCallback(toConfigKeyValueNode(dispatch));
+  }
+
+  return false;
+}
+
+bool tryParseConfig(char* data, int64 dataLength, const ConfigKeyValueNodeCallback& nodeCallback)
+{
+  if (!data || dataLength <= 0)
+  {
+    return false;
+  }
+
+  return libconfiniParse(data, dataLength, parseKeyValueCallback, (void*)&nodeCallback);
+}
+
+struct SectionOrKeyValueCallbackContext
+{
+  const ConfigSectionNodeCallback& sectionNodeCallback;
+  const ConfigKeyValueNodeCallback& keyValueNodeCallback;
+};
+static int parseSectionOrKeyValueCallback(IniDispatch* dispatch, void* userData)
+{
+  const SectionOrKeyValueCallbackContext& context = *(SectionOrKeyValueCallbackContext*)userData;
+
+  switch (dispatch->type)
+  {
+    case INI_KEY:
+    {
+      return context.keyValueNodeCallback(toConfigKeyValueNode(dispatch));
+    }
+
+    case INI_SECTION:
+    {
+      ConfigSectionNode node;
+      node.value = dispatch->value;
+      node.sectionPath = dispatch->append_to;
+      node.dataLength = dispatch->d_len;
+      node.valueLength = dispatch->v_len;
+      node.sectionPathLength = dispatch->at_len;
+
+      return context.sectionNodeCallback(node);
+    }
+
+    default:
+      return false;
+  }
+}
+
+bool tryParseConfig(char* data, int64 dataLength, const ConfigSectionNodeCallback& sectionNodeCallback, const ConfigKeyValueNodeCallback& keyValueNodeCallback)
+{
+  if (!data || dataLength <= 0)
+  {
+    return false;
+  }
+
+  SectionOrKeyValueCallbackContext context{ sectionNodeCallback, keyValueNodeCallback };
+  return libconfiniParse(data, dataLength, parseSectionOrKeyValueCallback, (void*)&context);
+}
