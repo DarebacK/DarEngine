8a4bcac1755d67fee1768978fe3d853e320aa333
DarebacK <dareback1@gmail.com>
2022-08-28 11:02:07 +0200

    Asset system design WIP

--- include/Core/Asset.hpp
+++ include/Core/Asset.hpp
@@ -4,11 +4,12 @@
 #include <mutex>
 #include <queue>
 
-#include "Core.hpp"
+#include "Core/Core.hpp"
 #include "Core/Memory.hpp"
+#include "Core/Task.hpp"
 
-// TODO: create an AssetRegistry inside the manager, that knows about all assets in the asset folder.
-// The registry will be filled during game start on the asset thread. 
+// TODO: create an AssetRegistry, that knows about all assets in the asset folder.
+// The registry will be filled during game start. 
 // The registry data structure will be static for the lifetime of the process, so no need to guard it against concurrent access,
 // although its content (the individual asset classes) may be accessed concurrently (TODO: do we really need this?).
 // The data structure will be organized into a tree, where a file directory is a node.
@@ -18,7 +19,30 @@
 // Individual assets/resources (Texture2D, Config) will be initialized based on a meta file that will be present next to the loaded asset file.
 // That meta file is separate from the asset file to allow VCS diffs. The meta file will be an INI file
 
-// TODO: Integrate libconfini
+void initializeAssetSystem();
+
+class AssetDirectoryRef
+{
+public:
+
+  AssetDirectoryRef(const AssetDirectoryRef& other);
+  AssetDirectoryRef(AssetDirectoryRef&& other);
+  ~AssetDirectoryRef();
+  AssetDirectoryRef& operator=(const AssetDirectoryRef& other);
+  AssetDirectoryRef& operator=(AssetDirectoryRef&& other);
+
+  // Completed when all assets in asset directory subtree are loaded and initialized.
+  Ref<TaskEvent> loadedEvent;
+
+private:
+
+  AssetDirectoryRef(class AssetDirectory* inDirectory) : directory(inDirectory) {}
+  friend AssetDirectoryRef loadAssetDirectory(const wchar_t* path);
+
+  class AssetDirectory* directory = nullptr;
+};
+// Increases the ref count of assets in the directory and it's subdirectories, potentially causing their async loading and initialization.
+AssetDirectoryRef loadAssetDirectory(const wchar_t* path);
 
 // TODO: Asset types could be defines in a AssetType.inl with macros, where the macro can once be used here to define enum values and also in ~Asset()
 enum class AssetType : uint16
@@ -26,8 +50,6 @@ enum class AssetType : uint16
   Unknown = 0,
   Config = 1,
   Texture2D = 100,
-  VertexShader = 200,
-  PixelShader = 201
 };
 
 class Asset
@@ -35,7 +57,7 @@ class Asset
 
 public:
 
-  AssetType getAssetType() const { return type; }
+  AssetType assetType = AssetType::Unknown;
 
   void ref();
   void unref();
@@ -50,7 +72,6 @@ protected:
 private:
 
   std::atomic<int32> refCount = 0;
-  AssetType type = AssetType::Unknown;
 };
 
 class Texture2D : public Asset
--- modules/Core/source/Asset.cpp
+++ modules/Core/source/Asset.cpp
@@ -1,3 +1,26 @@
 #include "Core/Asset.hpp"
 
 #include "Core/File.hpp"
+
+struct AssetDirectory
+{
+  std::wstring name;
+  std::vector<AssetDirectory> directories;
+  std::vector<Asset*> assets;
+};
+
+AssetDirectory rootDirectory;
+
+void initializeAssetSystem()
+{
+  // TODO: traverse the assets directory and create the AssetDirectory tree.
+  //FindFirstFile()
+}
+
+AssetDirectoryRef loadAssetDirectory(const wchar_t* path)
+{
+  // TODO: Traverse the subtree, increase ref count and add each assets laoded task event to an array, 
+  // which will be a prerequisite to a task that sets the returned task event ref to completed.
+
+  return AssetDirectoryRef(nullptr);
+}
