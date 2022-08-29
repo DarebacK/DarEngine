b9cb6a5344cb8905f60a3cce32a95b14eaa67fa5
DarebacK <dareback1@gmail.com>
2022-08-29 21:43:12 +0200

    Integrated DirectX DDSTextureLoader11.

--- include/Core/D3D11.hpp
+++ include/Core/D3D11.hpp
@@ -3,9 +3,12 @@
 #include <d3d11_4.h>
 #include <atlbase.h>
 
+#include "Core/Core.hpp"
+
 namespace D3D11
 {
   extern CComPtr<ID3D11Device> device;
   extern CComPtr<ID3D11DeviceContext> context;
 }
 
+HRESULT createTextureFromDDS(const uint8* ddsData, uint64 ddsDataSize, ID3D11Resource** texture, ID3D11ShaderResourceView** textureView);
--- modules/Core/source/D3D11.cpp
+++ modules/Core/source/D3D11.cpp
@@ -1,1 +1,12 @@
+#define DAR_MODULE_NAME "D3D11"
+
 #include "Core/D3D11.hpp"
+
+#include "external/DirextXTex/DDSTextureLoader11.h"
+
+using namespace D3D11;
+
+HRESULT createTextureFromDDS(const uint8* ddsData, uint64 ddsDataSize, ID3D11Resource** texture, ID3D11ShaderResourceView** textureView)
+{
+  return DirectX::CreateDDSTextureFromMemoryEx(device, ddsData, ddsDataSize, 0, D3D11_USAGE_IMMUTABLE, D3D11_BIND_SHADER_RESOURCE, 0, 0, DirectX::DDS_LOADER_DEFAULT, texture, textureView);
+}
