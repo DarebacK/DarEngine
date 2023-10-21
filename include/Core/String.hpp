56d41e5fabdce2568c367885aa22cdf91646bb9a
Pavel Morcinek <dareback1@gmail.com>
2023-10-21 13:06:22 +0200

    Added string isEqual helper function

--- include/Core/String.hpp
+++ include/Core/String.hpp
@@ -4,3 +4,5 @@
 
 // Combines strings into the destination buffer, doesn't null terminate.
 int64 combine(const wchar_t* string1, wchar_t char2, const wchar_t* string3, wchar_t* destination);
+
+inline bool isEqual(const wchar_t* string1, const wchar_t* string2) { return wcscmp(string1, string2) == 0; }
